add483097fc1642b5da718570bbdee8cdcb2251f
alexey.krylov <turn2river@gmail.com>
2021-08-09 00:14:16 +0300

    feat: add comments

--- src/index.js
+++ src/index.js
@@ -29,45 +29,46 @@ module.exports = function toReadable(number) {
         90: "ninety",
     };
 
-    let arrNumber = number
-        .toString().split("").map((num) => +num);
+    let arrNumber = number.toString().split("").map((num) => +num);
 
     switch (arrNumber.length) {
         case 1:
             if (number === 0) {
                 return "zero";
             }
-
+            //units
             return numWords[number];
 
         case 2:
+            //teens: 10 - 19
             if (number < 20 && number > 9) {
                 return numWords[number];
             }
-
+            // perfect tens
             if (number % 10 === 0) {
                 return numWords[number];
             }
-
+            //all other tens + units
             return `${numWords[arrNumber[0] * 10]} ${numWords[arrNumber[1]]}`;
 
         case 3:
+            //perfect hundreds
             if (arrNumber[1] === 0 && arrNumber[2] === 0) {
                 return `${numWords[arrNumber[0]]} hundred`;
             }
-
+            //hundreds + units. f.e.: 201, 202, 203..etc 
             if (arrNumber[1] === 0) {
                 return `${numWords[arrNumber[0]]} hundred ${numWords[arrNumber[2]]}`;
             }
-
+            //hundreds and teens
             if (arrNumber[1] === 1) {
                 return `${numWords[arrNumber[0]]} hundred ${numWords[10 + arrNumber[2]]}`;
             }
-
+            //hundresds and perfect tens
             if (arrNumber[1] >= 2 && arrNumber[2] === 0) {
                 return `${numWords[arrNumber[0]]} hundred ${numWords[arrNumber[1] * 10]}`;
             }
-
+            //all other hundreds + tens + units
             if (arrNumber[1] >= 2 && arrNumber[2] !== 0) {
                 return `${numWords[arrNumber[0]]} hundred ${numWords[arrNumber[1] * 10]} ${numWords[arrNumber[2]]}`;
             }
